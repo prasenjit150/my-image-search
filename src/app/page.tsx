cfd08ca4ad1fe0351de114d2f258378fccbd640b
Prasenjit <prasenjit@brenolabs.com>
2024-07-14 02:12:21 +0530

    Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,73 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import React, { useState } from 'react';
+import SearchBar from '../components/SearchBar';
+import ImageList from '../components/ImageList';
+import { fetchImages } from '../utils/api';
+import { Image } from '@/types/search';
+import Navbar from '@/components/Navbar';
+import Tabs from '@/components/Tabs';
+import Pagination from '@/components/Pagination';
 
-export default function Home() {
+const HomePage: React.FC = () => {
+  const [images, setImages] = useState<Image[]>([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(12);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSearch = async (query: string) => {
+    setQuery(query);
+    setPage(1);
+    try {
+      setIsLoading(true);
+      const data = await fetchImages(query, 1, perPage);
+      setImages(data.photos);
+    } catch (error) {
+      console.log('error', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleLoadMore = async () => {
+    const nextPage = page + 1;
+    try {
+      setIsLoading(true);
+      const data = await fetchImages(query, nextPage, perPage);
+      setImages([...images, ...data.photos]);
+      setPage(nextPage);
+    } catch (error) {
+      console.log('error', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className='main_container'>
+      <SearchBar onSearch={handleSearch} />
+      {images?.length > 0 ? (
+        <>
+          <Tabs />
+          <ImageList images={images} query={query} />
+          <div className='next_btn_container'>
+            <button className='next_page_btn' onClick={handleLoadMore}>
+             {isLoading ? "Loading..." : "Load more"}
+            </button>
+          </div>
+          <div className='bottom_pagination flex_center'>
+            <p className='bottom_pagination_text'>
+              Search Results for <span>{query}</span> Stock Photos and Images
+              &#40;1,652,108&#41;
+            </p>
+            <Pagination />
+          </div>
+        </>
+      ) : (
+        <div className='no_img_text'>{isLoading ? "Loading...": "Type and hit enter to see the images!"}</div>
+      )}
     </main>
   );
-}
+};
+
+export default HomePage;
